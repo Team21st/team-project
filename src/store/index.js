5bef0038ec62f6eeea7b105e336d39fc75cba475
MoMeak9 <1308994506@qq.com>
2021-09-12 00:26:31 +0800

    feat:初始化项目架构
    - 构建项目基本框架

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import getters from './getters'
+import settings from './modules/settings'
+import user from './modules/user'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+const store = new Vuex.Store({
   modules: {
-  }
+    settings,
+    user
+  },
+  getters
 })
+
+export default store
