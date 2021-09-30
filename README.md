f4ee9ff31e22b8a28ac27919b59e410715f701dc
MoMeak9 <1308994506@qq.com>
2021-09-30 23:50:34 +0800

    feat(Home):start Project

--- README.md
+++ README.md
@@ -1,25 +1,33 @@
 # team-project
-## 
+
+##  
+
 FuZhou University MIEC Team Project
+
+Yihui Shi, Yichen Hu
 
 ## Technology stack
 
 ## Project setup
+
 ```
 yarn install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 yarn serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 yarn build
 ```
 
 ### Lints and fixes files
+
 ```
 yarn lint
 ```
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,24 @@
 <template>
   <div class="home">
-    <el-button>ddd</el-button>
+    <div>
+      <div class="top-bar">
+        <div>
+          <el-image>
+
+          </el-image>
+          <a>sss</a>
+        </div>
+        <el-input
+          style="width: 200px;position: absolute;right: 20px"
+          suffix-icon="el-icon-date"
+          prefix-icon="el-icon-search"
+        ></el-input>
+      </div>
+    </div>
+    <el-row>
+      <el-col></el-col>
+      <el-col></el-col>
+    </el-row>
   </div>
 </template>
 
@@ -8,6 +26,17 @@
 
 export default {
   name: 'Home',
+  data() {
+    return {}
+  },
   components: {}
 }
 </script>
+
+<style scoped lang="less">
+.top-bar {
+  display: flex;
+  margin-top: 20px;
+  position: relative;
+}
+</style>
