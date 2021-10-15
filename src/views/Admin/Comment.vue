a8e3e0ccd9f4b660df2a6e6a1c95a9877bae73eb
MoMeak9 <1308994506@qq.com>
2021-10-15 14:42:46 +0800

    feat(Admin):language for English

--- src/views/Admin/Comment.vue
+++ src/views/Admin/Comment.vue
@@ -4,12 +4,12 @@
       <el-table-column prop="userBean.userName" label="用户名"></el-table-column>
       <el-table-column prop="userBean.userEmail" label="邮箱"></el-table-column>
       <el-table-column prop="userBean.userNo" label="UUID" width="180"></el-table-column>
-      <el-table-column prop="userBean.comment" label="评论内容" width="400"></el-table-column>
-      <el-table-column prop="userBean.reason" label="举报原因" width="300"></el-table-column>
+      <el-table-column prop="userBean.comment" label="Content" width="400"></el-table-column>
+      <el-table-column prop="userBean.reason" label="Reason" width="300"></el-table-column>
       <el-table-column label="账号操作">
-        <el-button type="danger">封禁用户
+        <el-button type="danger">Ban
         </el-button>
-        <el-button type="info">删除
+        <el-button type="info">Delete
         </el-button>
       </el-table-column>
     </el-table>
