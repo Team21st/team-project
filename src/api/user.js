bdf1f66dcd21f94566f55bb98ee02eb5de1d35f1
MoMeak9 <1308994506@qq.com>
2021-10-24 15:51:13 +0800

    feat(API):new API for users

--- src/api/user.js
+++ src/api/user.js
@@ -10,6 +10,45 @@ export function changeUserPassword(params) {
   })
 }
 
+export function forgetUserPassword(params) {
+  return request({
+    requestServer: 'StoreService',
+    url: '/user/forgetUserPassword',
+    method: 'post',
+    data: params
+  })
+}
+
+export function queryUserPrivateInfo(params) {
+  return request({
+    requestServer: 'StoreService',
+    url: '/user/queryUserPrivateInfo',
+    method: 'post',
+    data: params
+  })
+}
+
+export function updateUserPrivateInfo(params) {
+  return request({
+    requestServer: 'StoreService',
+    url: '/user/updateUserPrivateInfo',
+    method: 'post',
+    data: params
+  })
+}
+
+export function uploadHeadPortrait(params) {
+  return request({
+    headers: {
+      'Content-Type': 'multipart/form-data'
+    },
+    requestServer: 'StoreService',
+    url: '/user/uploadHeadPortrait',
+    method: 'post',
+    data: params
+  })
+}
+
 export function userLogin(params) {
   return request({
     requestServer: 'StoreService',
