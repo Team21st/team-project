53115325c6e853c71789b63cd7351658da42a74f
MoMeak9 <1308994506@qq.com>
2021-11-09 17:25:04 +0800

    feat(personal):UserPersonal Center

--- src/components/UserCenter/receiveOrder.vue
+++ src/components/UserCenter/receiveOrder.vue
@@ -1,10 +1,39 @@
 <template>
-  <div id="receive-order"></div>
+  <div id="receive-order">
+    <el-table
+      style="margin-top: 10px"
+      load="loading"
+      data="">
+      <el-table-column
+        v-for="item in tableList"
+        :key="item.label"
+        :label="item.label"
+        :width="item.width"
+        :prop="item.prop">
+      </el-table-column>
+      <el-table-column
+        label="Operation">
+        <template slot-scope="scope">
+          <el-button v-if="scope.row.status" type="primary">
+
+          </el-button>
+          <el-button v-if="scope.row.status" type="danger">
+
+          </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "receiveOrder.vue"
+  name: "receiveOrder.vue",
+  data() {
+    return {
+      tableList: []
+    }
+  }
 }
 </script>
 
--- src/components/UserCenter/userSellBook.vue
+++ src/components/UserCenter/userSellBook.vue
@@ -1,10 +1,39 @@
 <template>
-  <div id="user-selling"></div>
+  <div id="user-selling">
+    <el-table
+      style="margin-top: 10px"
+      load="loading"
+      data="">
+      <el-table-column
+        v-for="item in tableList"
+        :key="item.label"
+        :label="item.label"
+        :width="item.width"
+        :prop="item.prop">
+      </el-table-column>
+      <el-table-column
+        label="Operation">
+        <template slot-scope="scope">
+          <el-button v-if="scope.row.status" type="primary">
+
+          </el-button>
+          <el-button v-if="scope.row.status" type="danger">
+
+          </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "userSellBook"
+  name: "userSellBook",
+  data() {
+    return {
+      tableList: []
+    }
+  }
 }
 </script>
 
